69c7a781b2f96a01422ba1501fcd159783235768
Ricky <ricky@siriusdao.org>
2022-08-07 22:16:24 +0800

    feat: add api/ticker/24hr

---
+++ pages/api/ticker/24hr.js
@@ -0,0 +1,94 @@
+import { utils, constants } from 'ethers'
+const { parseUnits, formatUnits } = utils
+const { Zero } = constants
+import moment from 'moment'
+import { pairs, graphUrl } from '../../../constants'
+import { fn, getCoinIndex, getCoinDecimals, castTo18 } from '../../../utils'
+
+const getTicker = async ticker_id => {
+  if (!ticker_id) throw 'ticker_id is required'
+
+  const pair = pairs.find(i => i.ticker_id == ticker_id)
+  if (!pair) throw 'pair is not found by ticker_id'
+
+  const pool_id = pair.pool_id.toLowerCase()
+  const base_id = getCoinIndex(pool_id, pair.base)
+  const target_id = getCoinIndex(pool_id, pair.target)
+
+  let ticker = {
+    pool_id,
+    ticker_id,
+    base_currency: pair.base,
+    target_currency: pair.target
+  }
+
+  const yesterday = moment().subtract(1, 'd').unix()
+  const query = `{
+    tokenExchanges(orderBy: timestamp, orderDirection: desc, first: 1000, where: {
+      swap: "${pool_id}",
+      soldId: ${base_id},
+      boughtId: ${target_id},
+      timestamp_gte: ${yesterday}
+    }) {
+      soldId
+      boughtId
+      tokensSold
+      tokensBought
+      timestamp
+      swap { id }
+    }
+  }`
+  const result = await (await fetch(graphUrl, { method: 'POST', body: JSON.stringify({ query }) })).json()
+  const tokenExchanges = result?.data?.tokenExchanges
+  if (!tokenExchanges?.length) return ticker
+
+  const { tokensSold, tokensBought } = tokenExchanges[0]
+  const decimal0 = getCoinDecimals(pair.base)
+  const decimal1 = getCoinDecimals(pair.target)
+
+  const base_bn = castTo18(tokensSold, decimal0)
+  const target_bn = castTo18(tokensBought, decimal1)
+  const last_price = base_bn.isZero() ? Zero : parseUnits('1').mul(target_bn).div(base_bn)
+
+  let base_volume = Zero
+  let target_volume = Zero
+  let high = Zero
+  let low = null
+
+  tokenExchanges.map(i => {
+    const base_bn = castTo18(i.tokensSold, decimal0)
+    const target_bn = castTo18(i.tokensBought, decimal1)
+    base_volume = base_volume.add(base_bn)
+    target_volume = target_volume.add(target_bn)
+    const price = base_bn.isZero() ? Zero : parseUnits('1').mul(target_bn).div(base_bn)
+    if (price.gt(high)) high = price
+    if (!low || price.lt(low)) low = price
+  })
+  if (!low) low = Zero
+
+  ticker = {
+    ...ticker,
+    last_price: formatUnits(last_price),
+    base_volume: formatUnits(base_volume),
+    target_volume: formatUnits(target_volume),
+    high: formatUnits(high),
+    low: formatUnits(low)
+  }
+
+  return ticker
+}
+
+export default fn(
+  async ({ ticker_id }) => {
+    if (ticker_id) {
+      const ticker = await getTicker(ticker_id)
+      return { tickers: [ticker] }
+    }
+
+    const tickers = await Promise.all(pairs.map(i => getTicker(i.ticker_id)))
+    return { tickers }
+  },
+  {
+    maxAge: 60 // 1 min
+  }
+)
--- utils/index.js
+++ utils/index.js
@@ -1,1 +1,23 @@
+import { utils } from 'ethers'
+const { parseUnits } = utils
+import { pools, coins } from '../constants'
+
 export * from './api'
+
+export const getCoinIndex = (pool_id, symbol) => {
+  const pool = pools.find(i => (i.addresses.deposit || i.addresses.swap).toLowerCase() == pool_id.toLowerCase())
+  if (!pool) throw 'pool is not found by pool_id'
+  return pool.coins.findIndex(i => i.symbol == symbol)
+}
+
+export const getCoinDecimals = symbol => {
+  const coin = Object.values(coins).find(i => i.symbol == symbol)
+  if (!coin) throw 'coin is not found by symbol'
+  return coin.decimals
+}
+
+// cast to 18 decimals
+export const castTo18 = (bn, decimals) =>
+  parseUnits('10', 0)
+    .pow(18 - decimals)
+    .mul(bn)
