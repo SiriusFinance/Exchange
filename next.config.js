2066d06b7581ec2f32aa99935c20c707edfbbe91
Ricky <ricky@siriusdao.org>
2022-08-11 16:54:20 +0800

    fix: api cors

---
+++ next.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          { key: 'Access-Control-Allow-Origin', value: '*' },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,POST,OPTIONS,HEAD' }
+        ]
+      }
+    ]
+  }
+}
