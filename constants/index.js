83ee775cca7e71ad068769e5e3c1e719af1132a7
Ricky <ricky@siriusdao.org>
2022-10-11 21:28:50 +0800

    chore: update subsquid version

--- constants/index.js
+++ constants/index.js
@@ -5,5 +5,5 @@ import coins from './coins'
 export { pools, pairs, coins }
 
 export const GECKO_URL = 'https://api.coingecko.com/api/v3'
-export const GRAPH_URL = 'https://squid.subsquid.io/sirius/v/v7/graphql'
+export const GRAPH_URL = 'https://squid.subsquid.io/sirius/v/v8/graphql'
 export const RPC_URL = 'https://rpc.pinknode.io/astar/7fb80172-02cb-4173-a505-91b75ccdd9f1'
