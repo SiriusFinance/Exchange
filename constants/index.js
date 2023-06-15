28b87cbed2121e1a9cc19359fad96df32ff73f2b
Ronald <ronaldpapandreagwy66@gmail.com>
2023-06-15 16:02:14 +0800

    Fix RPC endpoint to blast.io

--- constants/index.js
+++ constants/index.js
@@ -6,4 +6,4 @@ export { pools, pairs, coins }
 
 export const GECKO_URL = 'https://api.coingecko.com/api/v3'
 export const GRAPH_URL = 'https://squid.subsquid.io/sirius/v/v8/graphql'
-export const RPC_URL = 'https://rpc.pinknode.io/astar/7fb80172-02cb-4173-a505-91b75ccdd9f1'
+export const RPC_URL = 'https://astar.blastapi.io/79dfcf6c-4455-4cc9-b58f-54270af1ac93'
